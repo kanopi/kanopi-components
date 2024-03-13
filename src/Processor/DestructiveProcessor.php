49ea28b06e1f25067a6bfc3055ac5f9e8cba13f6
Ryan Leeson <ryan.leeson@kanopistudios.com>
2024-03-13 19:43:37 -0400

    - Revert DestructiveProcessor change, it's wanted

--- src/Processor/DestructiveProcessor.php
+++ src/Processor/DestructiveProcessor.php
@@ -14,7 +14,7 @@ use Kanopi\Components\Services\System\ITrackingIndex;
  * @package kanopi/components
  */
 trait DestructiveProcessor {
-	use RecurrentProcessor {
+	use CoreProcessor {
 		preProcessEvents as corePreProcessEvents;
 	}
 
