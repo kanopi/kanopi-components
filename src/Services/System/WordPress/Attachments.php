617a8c3d1deaa42c1286c14f910bc499296cb24c
Ryan Leeson <ryan.leeson@kanopistudios.com>
2024-03-20 20:59:07 -0400

    - Change attachement service to do a LIKE comparison to search for attached files

--- src/Services/System/WordPress/Attachments.php
+++ src/Services/System/WordPress/Attachments.php
@@ -4,6 +4,7 @@ namespace Kanopi\Components\Services\System\WordPress;
 
 use Kanopi\Components\Model\Data\WordPress\Attachment;
 use Kanopi\Components\Model\Data\WordPress\IPostTypeEntity;
+use Kanopi\Components\Model\Exception\SetReaderException;
 use Kanopi\Components\Repositories\IIndexedEntityGroupWriter;
 use Kanopi\Components\Repositories\ISetReader;
 use Kanopi\Components\Repositories\ISetWriter;
@@ -72,6 +73,37 @@ class Attachments implements IIndexedEntityWriter {
 	 */
 	protected function metaDataRepository(): ISetReader {
 		return $this->metaDataRepository;
+	}
+
+	/**
+	 * Read an entity by a unique identifier
+	 *    - Assumes the Unique Identifier is in a meta field named by uniqueIdentifierFieldName
+	 *    - Override this function if it does not fit the use case
+	 *
+	 * @param string $_unique_identifier Entity index identifier
+	 *
+	 * @return IPostTypeEntity|null
+	 * @throws SetReaderException Unable to read system entity
+	 */
+	public function readByUniqueIdentifier( string $_unique_identifier ): ?IPostTypeEntity {
+		$post_cursor = $this->entityRepository()->read(
+			[
+				'post_status'    => $this->allowedIndexPostStatus(),
+				'post_type'      => $this->systemEntityName(),
+				// phpcs:ignore -- Intentional meta data query
+				'meta_query'     => [
+					[
+						'key'     => $this->uniqueIdentifierFieldName(),
+						'value'   => $_unique_identifier,
+						'compare' => 'LIKE',
+					],
+				],
+				'posts_per_page' => 1,
+				'fields'         => 'all',
+			]
+		);
+
+		return $post_cursor->valid() ? $this->readSystemEntity( $post_cursor->current() ) : null;
 	}
 
 	/**
